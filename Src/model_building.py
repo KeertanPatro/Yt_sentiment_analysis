c77da673919d7b18bd1d42e0ad2a1b71f989e930
KeertanPatro <keertanpatro97@gmail.com>
2026-01-27 10:05:44 +0530

    Adding buildspec.yml file

--- Src/model_building.py
+++ Src/model_building.py
@@ -3,7 +3,6 @@ import os
 import logging
 import json
 import boto3
-# 427521931174.dkr.ecr.us-east-1.amazonaws.com/yt_sentiment_analysis
 
 
 s3_client= boto3.client('s3',aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),region_name='us-east-1')
